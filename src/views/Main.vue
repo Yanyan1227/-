9d6808507df9ef287bb8608d8660340581bb2750
yinyan1227 <yinyan2323@qq.com>
2024-03-16 16:04:47 +0800

    Changes

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,3 +1,11 @@
 <template>
-  测试的
+  <div class="common-layout">
+    <el-container>
+      <el-aside width="200px" class="menus">Aside</el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
